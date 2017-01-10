0f39d6cb283363afafb939e10862b1ec6d76dc9d
frenchdonuts <jonathantan1991@gmail.com>
2017-01-10 01:12:31 -0500

    feat: Fetch issues over time data

--- src/elm/KeyMetrics.elm
+++ src/elm/KeyMetrics.elm
@@ -1,25 +1,37 @@
 module KeyMetrics exposing (..)
 
 import Html as H exposing (Html)
+import Http
+import Json.Decode as J
+import RemoteData as R
 
 
 type alias Model =
-    { openIssuesCount : Int
-    , payingCustomersOverTimeData : List ( Float, String )
-    , issuesOverTimeData : List ( Float, String )
+    { payingCustomersOverTimeData : List ( Float, String )
+    , issuesOverTimeData : R.RemoteData Http.Error (List ( Float, String ))
     }
 
 
 type Msg
     = NoOp
+    | IssuesOverTimeDataFetched (R.RemoteData Http.Error (List Float))
 
 
 init : Model
 init =
-    { openIssuesCount = 0
-    , payingCustomersOverTimeData = []
-    , issuesOverTimeData = []
+    { payingCustomersOverTimeData = []
+    , issuesOverTimeData = R.NotAsked
     }
+
+
+fetchIssuesOverTimeData : Cmd Msg
+fetchIssuesOverTimeData =
+    let
+        decoder =
+            J.succeed [ 4.2 ]
+    in
+        Http.get "http://localhost:8080/static/key_metrics.json" decoder
+            |> Http.send (R.fromResult >> IssuesOverTimeDataFetched)
 
 
 update : Msg -> Model -> Model
@@ -28,11 +40,42 @@ update msg model =
         NoOp ->
             model
 
+        IssuesOverTimeDataFetched remoteData ->
+            { model | issuesOverTimeData = R.map (List.indexedMap (\i f -> ( f, toMonth i ))) remoteData }
+
+
+toMonth : Int -> String
+toMonth i =
+    if i + 1 % 12 == 0 then
+        "Dec"
+    else if i + 1 % 11 == 0 then
+        "Nov"
+    else if i + 1 % 10 == 0 then
+        "Oct"
+    else if i + 1 % 9 == 0 then
+        "Sep"
+    else if i + 1 % 8 == 0 then
+        "Aug"
+    else if i + 1 % 7 == 0 then
+        "Jul"
+    else if i + 1 % 6 == 0 then
+        "Jun"
+    else if i + 1 % 5 == 0 then
+        "May"
+    else if i + 1 % 4 == 0 then
+        "Apr"
+    else if i + 1 % 3 == 0 then
+        "Mar"
+    else if i + 1 % 2 == 0 then
+        "Feb"
+    else
+        "Jan"
+
 
 view : Model -> Html Msg
 view model =
     let
-        { openIssuesCount, payingCustomersOverTimeData, issuesOverTimeData } =
+        { payingCustomersOverTimeData, issuesOverTimeData } =
             model
     in
         H.div [] []
